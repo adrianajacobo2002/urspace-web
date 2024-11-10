8a30fb4be0f0dd3cca017f0debc988d7d3a99392
Adriana Jacobo <adrianajacobo35@gmail.com>
2024-11-10 17:46:29 -0600

    dashboard banner

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,8 +1,10 @@
 // src/pages/Dashboard.tsx
-import React from 'react';
-import { Box, Typography, Button, keyframes } from '@mui/material';
-import ParticlesBackground from '../../components/ParticleBg';
-import AstronautImage from '../../assets/img/astronauta_explorando.svg'; // Imagen del astronauta
+import React from "react";
+import { Box, Typography, Button, keyframes } from "@mui/material";
+import ParticlesBackground from "../../components/ParticleBg";
+import AstronautImage from "../../assets/img/astronauta_explorando.svg";
+import Navbar from "../../layouts/UserNavbar";
+import PlanetImage from "../../assets/img/planeta.png"; // Imagen del planeta
 
 // Definición de la animación de flotación
 const floatAnimation = keyframes`
@@ -19,49 +21,72 @@ const floatAnimation = keyframes`
 
 const Dashboard: React.FC = () => {
   return (
-    <Box sx={{ backgroundColor: '#fff', minHeight: '100vh', position: 'relative', overflow: 'hidden' }}>
+    <Box
+      sx={{
+        backgroundColor: "#fff",
+        minHeight: "100vh",
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
       {/* Contenedor con fondo ondulado y partículas */}
+      <Navbar />
       <Box
         sx={{
-          backgroundColor: '#04172b',
-          minHeight: '60vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          color: '#fff',
-          textAlign: 'center',
-          position: 'relative',
+          backgroundColor: "#04172b",
+          minHeight: "70vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          color: "#fff",
+          textAlign: "center",
+          position: "relative",
           zIndex: 1,
           padding: 4,
-          clipPath: 'ellipse(80% 100% at 50% 0%)', // Ajusta esta propiedad para dar forma ondulada
+          clipPath: "ellipse(80% 100% at 50% 0%)", // Ajusta esta propiedad para dar forma ondulada
         }}
       >
         {/* Partículas de fondo en esta sección */}
         <ParticlesBackground />
 
-        {/* Contenido del dashboard */}
-        <Typography variant="h3" sx={{ fontWeight: 'bold', mb: 1 }}>
-          Bienvenido Usuario
-        </Typography>
-        <Typography variant="subtitle1" sx={{ mb: 3 }}>
-          Es momento de explorar
-        </Typography>
+        <Box sx={{ position: "relative", zIndex: 3 }}>
+          <Typography variant="h3" sx={{ fontWeight: "bold", mb: 1 }}>
+            Bienvenido Usuario
+          </Typography>
+          <Typography variant="subtitle1" sx={{ mb: 3 }}>
+            Es momento de explorar
+          </Typography>
 
-        {/* Botones */}
-        <Box sx={{ display: 'flex', gap: 2 }}>
-          <Button
-            variant="contained"
-            sx={{ backgroundColor: '#65348c', '&:hover': { backgroundColor: '#4e278c' } }}
+          {/* Botones */}
+          <Box
+            sx={{ display: "flex", justifyContent: "center", gap: 2, mt: 5 }}
           >
-            Buscar Terreno
-          </Button>
-          <Button
-            variant="contained"
-            sx={{ backgroundColor: '#65348c', '&:hover': { backgroundColor: '#4e278c' } }}
-          >
-            Publicar Terreno
-          </Button>
+            <Button
+              variant="contained"
+              sx={{
+                width: "200px",
+                backgroundColor: "#65348c",
+                "&:hover": { backgroundColor: "#4e278c" },
+                textTransform: "none",
+                fontSize: "1rem",
+              }}
+            >
+              Buscar Terreno
+            </Button>
+            <Button
+              variant="contained"
+              sx={{
+                width: "200px",
+                backgroundColor: "#65348c",
+                "&:hover": { backgroundColor: "#4e278c" },
+                textTransform: "none",
+                fontSize: "1rem",
+              }}
+            >
+              Publicar Terreno
+            </Button>
+          </Box>
         </Box>
 
         {/* Imagen del astronauta en la esquina superior derecha con animación de flotación */}
@@ -70,12 +95,26 @@ const Dashboard: React.FC = () => {
           src={AstronautImage}
           alt="Astronauta"
           sx={{
-            position: 'absolute',
-            top: '100px',
-            right: '230px',
-            maxWidth: '150px',
+            position: "absolute",
+            top: "150px",
+            right: "230px",
+            maxWidth: "200px",
             zIndex: 2,
             animation: `${floatAnimation} 3s ease-in-out infinite`, // Aplica la animación de flotación
+          }}
+        />
+
+        <Box
+          component="img"
+          src={PlanetImage}
+          alt="Planeta"
+          sx={{
+            position: "absolute",
+            top: "150px",
+            left: "200px",
+            maxWidth: "200px", // Ajusta el tamaño del planeta
+            zIndex: 2,
+            animation: `${floatAnimation} 4s ease-in-out infinite`, // Aplica la animación de flotación
           }}
         />
       </Box>
